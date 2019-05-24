f7ffb0ec03fec28b999f67a9f993975b5c8f767a
Mohsin <mohsin@mmcgbl.com>
2019-05-24 10:57:18 +0500

    added RTCmulticonnection

--- Server/node-js/io.js
+++ Server/node-js/io.js
@@ -1,17 +1,27 @@
 var io = require('socket.io')();
+var RTCMultiConnectionServer = require('rtcmulticonnection-server')
 const UserController = require('./controllers/user.controller')
+const port = normalizePort(CONFIG.port || '3000');
 
 io.on('connection', function (socket) {
     socket.on('set-online', (data) => {
         UserController.changeOnlineStatus(data, true);
-        io.emit('contact-online',data)
+        io.emit('contact-online', data)
     })
     socket.on('set-offline', data => {
         UserController.changeOnlineStatus(data, false);
-        io.emit('contact-offline',data)
+        io.emit('contact-offline', data)
     })
     socket.on('disconnect', (data) => {
         console.log('got dc', data)
+    })
+
+    RTCMultiConnectionServer.addSocket(socket, {
+        config: {
+            "socketMessageEvent": "thrive19-message",
+            "socketCustomEvent": "thrive19-custom-message",
+            "port": port
+        }
     })
 });
 
