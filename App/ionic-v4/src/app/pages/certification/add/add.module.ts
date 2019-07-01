51d0afdfc49918a545673e2a19c5678f4be05d81
noomaan <nomii09@hotmail.com>
2019-07-01 19:26:17 +0500

    add course Form ui inprogress

---
+++ App/ionic-v4/src/app/pages/certification/add/add.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AddPage } from './add.page';
+import { MatFormFieldModule, MatSelectModule } from '@angular/material';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AddPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    MatFormFieldModule,
+    MatSelectModule
+
+  ],
+  declarations: [AddPage]
+})
+export class AddPageModule {}
--- App/ionic-v4/src/app/pages/certification/certification-routing.module.ts
+++ App/ionic-v4/src/app/pages/certification/certification-routing.module.ts
@@ -4,8 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './certification/certification.module#CertificationPageModule' },
   { path: 'detail/:id', loadChildren: './certification-detail/certification-detail.module#CertificationDetailPageModule' },
-  { path: 'add', loadChildren: './certification-form/certification-form.module#CertificationFormPageModule' },
+  // { path: 'add', loadChildren: './certification-form/certification-form.module#CertificationFormPageModule' },
   { path: 'edit/:id', loadChildren: './certification-form/certification-form.module#CertificationFormPageModule' },
+  { path: 'add', loadChildren: './add/add.module#AddPageModule' },
+
 ];
 
 @NgModule({
