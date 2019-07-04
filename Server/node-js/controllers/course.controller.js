ec68323836ac403eb9ccd461eb3be903b4d8a14b
shameel1996 <sshameelakhtar@gmail.com>
2019-07-04 15:05:35 +0500

    New apis development

--- Server/node-js/controllers/course.controller.js
+++ Server/node-js/controllers/course.controller.js
@@ -1,7 +1,74 @@
-const { Course, Category } = require('../models');
+const { Course, Category, StudentCourse } = require('../models');
 const { to, ReE, ReS } = require('../services/util.service');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
+
+
+const enrollCourse = async (req, res) => {
+    let { userId, courseId } = req.body
+    const studentCourse = await StudentCourse.create({
+        status: 0,
+        UserId: userId,
+        CourseId: courseId,
+    })
+    if (studentCourse) return ReS(res, { data: studentCourse }, 200);
+    else return ReE(res, { message: 'Unable to insert Course.' }, 500)
+}
+module.exports.enrollCourse = enrollCourse;
+
+
+const getStudentCourse = async (req, res) => {
+    let { userId } = req.params
+    const studentCourseIds = await StudentCourse.findAll({
+        // attributes: [['CourseId', 'courseId']],
+        // include: [{
+        //     model: Course,
+        //     as: "course",
+        //     attributes: [['id', 'categoryId'], 'title', 'description', 'image', 'createdBy'],
+        //     where:{
+        //         courseId: courseId
+        //     }
+        // }],
+        where: {
+            UserId: userId
+        }
+    })
+
+    let courseIds = studentCourseIds.map(val=> val.CourseId);
+
+    console.log(studentCourseIds);
+
+    const studentCourses = await Course.findAll({
+        attributes: [['id', 'courseId'], 'title', 'description', 'image', 'createdby'],
+        include: [{
+            model: Category,
+            as: "category",
+            attributes: [['id', 'categoryId'], 'title']
+        }],
+        where:{
+            id:{
+                [Op.notIn]:courseIds
+            } 
+        }
+    })
+
+    if (studentCourses) return ReS(res, { data: studentCourses }, 200);
+    else return ReE(res, { message: 'Unable to get Course.' }, 500)
+}
+module.exports.getStudentCourse = getStudentCourse;
+
+
+const updateStudentCourse = async (req, res) => {
+    let { userId, courseId } = req.body
+    const studentCourse = await StudentCourse.create({
+        status: 1,
+        UserId: userId,
+        CourseId: courseId,
+    })
+    if (course) return ReS(res, { data: course }, 200);
+    else return ReE(res, { message: 'Unable to insert Course.' }, 500)
+}
+module.exports.updateStudentCourse = updateStudentCourse;
 
 
 const create = async (req, res) => {
