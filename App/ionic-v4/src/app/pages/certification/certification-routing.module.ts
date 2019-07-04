023c0073b472a534a6c71d1b6548091181fc086c
noomaan <nomii09@hotmail.com>
2019-07-04 16:31:16 +0500

    section ui inprogress

--- App/ionic-v4/src/app/pages/certification/certification-routing.module.ts
+++ App/ionic-v4/src/app/pages/certification/certification-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: 'edit/:id', loadChildren: './certification-form/certification-form.module#CertificationFormPageModule' },
   { path: 'add', loadChildren: './add/add.module#AddPageModule' },  { path: 'addmodule', loadChildren: './addmodule/addmodule.module#AddmodulePageModule' },
   { path: 'moduledetail', loadChildren: './moduledetail/moduledetail.module#ModuledetailPageModule' },
+  { path: 'sections', loadChildren: './sections/sections.module#SectionsPageModule' },
 
 
 ];
