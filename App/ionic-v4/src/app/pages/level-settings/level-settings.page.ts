b0041ab8dd6e3172bc2358131bccc4d95b54cfc8
mohsin <mohsin@mmcgbl.com>
2019-07-05 19:37:03 +0500

    level-setting-page

--- App/ionic-v4/src/app/pages/level-settings/level-settings.page.ts
+++ App/ionic-v4/src/app/pages/level-settings/level-settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from '../../services/http/rest-api.service';
 
 @Component({
   selector: 'app-level-settings',
@@ -7,15 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LevelSettingsPage implements OnInit {
 
-  initialLevel : string = '';
-  initialExperience : string = '';
+  initialLevel: string = '';
+  initialExperience: string = '';
+  baseUrl: string;
 
-  constructor() { }
+  constructor(private restApi: RestApiService) {
+    this.baseUrl = this.restApi.url;
+  }
 
   ngOnInit() {
   }
 
-  saveSettings(){
-    alert(this.initialExperience)
+  saveSettings() {
+    let obj = {
+      initialLevel: this.initialLevel,
+      initialExperience: this.initialExperience,
+      adminId: "b5a8abb3-ec1a-4abe-bd07-681da590c0c5"
+    }
+
+    this.restApi.post('studentExperienceSettings', obj, {}).subscribe((res: any) => {
+      if (res.success === true) {
+        console.log(res);
+      } else {
+        console.log(res);
+      }
+    });
   }
 }
