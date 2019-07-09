5da4eb0f7f80382d73c3640aff004850df69935f
noomaan <48157858+noomaan@users.noreply.github.com>
2019-07-09 03:31:39 -0700

    round progress ui circle

--- App/ionic-v4/src/app/pages/certification/addmodule/addmodule.module.ts
+++ App/ionic-v4/src/app/pages/certification/addmodule/addmodule.module.ts
@@ -5,11 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 
 import {RoundProgressModule} from 'angular-svg-round-progressbar';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 import { IonicModule } from '@ionic/angular';
 
 import { AddmodulePage } from './addmodule.page';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
   {
@@ -25,6 +26,22 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RoundProgressModule,
+    // BrowserModule,
+    NgCircleProgressModule.forRoot({
+      "backgroundGradient": true,
+      "backgroundColor": "#ffffff",
+      "backgroundGradientStopColor": "#c0c0c0",
+      "backgroundPadding": -10,
+      "radius": 40,
+      "maxPercent": 100,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#61A9DC",
+      "innerStrokeWidth": 0,
+      "subtitleColor": "#444444",
+      "showBackground": false,
+      "showInnerStroke": false,
+      "responsive": false
+    }),
     
     RouterModule.forChild(routes)
   ],
