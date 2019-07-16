47a675031f2b7b069bf6fbce4b2b8a3760954045
owaismmc <owais@mmcgbl.com>
2019-07-16 18:23:23 +0500

    envirnment set local and remote

--- App/ionic-v4/src/environments/environment.ts
+++ App/ionic-v4/src/environments/environment.ts
@@ -14,14 +14,18 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// local path
+// export const SERVER_URL = 'http://localhost:3000/v1/';
+// export const IMAGE_URL = 'http://localhost:3000/';
+// export const SOCKET_URL = 'http://localhost:3000/';
+// export const RTC_SIGNALLING_SERVER_URL = 'https://thrive19.com:9001/';
 
-//  export const SERVER_URL = 'https://thrive19.eastus.cloudapp.azure.com/v1/';
-export const SERVER_URL = 'http://localhost:3000/v1/';
-export const IMAGE_URL = 'http://localhost:3000/';
-//  export const SOCKET_URL = 'https://thrive19.eastus.cloudapp.azure.com/';
-export const SOCKET_URL = 'http://localhost:3000/';
+// remote path
+export const SERVER_URL = 'https://api.thrive19.com/v1/';
+export const SOCKET_URL = 'https://api.thrive19.com/';
+export const IMAGE_URL = 'https://api.thrive19.com/';
 export const RTC_SIGNALLING_SERVER_URL = 'https://thrive19.com:9001/';
-//export const RTC_SIGNALLING_SERVER_URL = 'https://rtcmulticonnection.herokuapp.com:443/';
+
 export const ICE_SERVERS = null;
 export const PREFIX_LGT = 'LGT';
 export const DEFAULT_TIMEZONE = 'Australia/Sydney';
