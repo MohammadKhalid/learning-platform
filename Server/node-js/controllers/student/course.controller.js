275c30091debffb927305ddd344fae6a8b8be781
owaismmc <owais@mmcgbl.com>
2019-07-31 15:53:15 +0500

    user side certification list populate service

--- Server/node-js/controllers/student/course.controller.js
+++ Server/node-js/controllers/student/course.controller.js
@@ -1,12 +1,64 @@
-const { Course, CourseCategory, StudentCourse } = require('../../models');
+const { Course, CourseCategory, StudentCourse, User } = require('../../models');
 const { to, ReE, ReS } = require('../../services/util.service');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 
 const getCourses = async (req, res) => {
+    let { adminId, categories, searchBy } = req.query;
+    let coachIds = await User.findAll({
+        where: { createdBy: adminId, type: 'coach' },
+        attributes: ['id']
+    });
 
-    console.log('here')
+    let condition = {}
+    let categoryCondition = {}
+    condition.createdBy = {
+        [Op.in]: coachIds.map(x => x.id)
+    }
+
+    if (categories) {
+        categories = categories.split(',')
+        categoryCondition.id = {
+            [Op.in]: categories
+        }
+    }
+
+    if (searchBy) {
+        condition.title = {
+            [Op.like]: `%${searchBy}%`
+        }
+    }
+
+    const course = await CourseCategory.findAll({
+        attributes: [['id', 'categoryId'], 'title'],
+        include: [{
+            model: Course,
+            as: "subCategories",
+            attributes: [['id', 'courseId'], 'title', 'description', 'image'],
+            where: condition
+        }],
+        where: categoryCondition
+    })
+    if (course) return ReS(res, { data: course }, 200);
+    else return ReE(res, { message: 'Unable to insert Course.' }, 500)
+    // let courses = await Course.findAll({
+    //     where: {
+    //         createdBy: {
+    //             [Op.in]: coachIds.map(x => x.id)
+    //         },
+    //         attributes:['id','title','description','image',]
+    //     },
+    //     include: [
+    //         {
+    //             model: User,
+    //             as: 'coach',
+    //             attributes: [[Sequelize.fn("CONCAT", Sequelize.col('firstName'), ' ', Sequelize.col('lastName')), "fullName"]]
+
+    //         }
+    //     ]
+    // });
+    // res.send(courses);
     // let { userId } = req.params
     // const studentCourseIds = await StudentCourse.findAll({
     //     // attributes: [['CourseId', 'courseId']],
