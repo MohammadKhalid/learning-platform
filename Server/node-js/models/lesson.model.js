4ccb06910ddaceda92fabba0586e46c8eb32535a
Mohsin <mohsin@mmcgbl.com>
2019-08-09 17:33:57 +0500

    sectionPages created

--- Server/node-js/models/lesson.model.js
+++ Server/node-js/models/lesson.model.js
@@ -22,14 +22,14 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
             defaultValue: "Lesson"
         },
-        sectionId: {
+        sectionPageId: {
             type: DataTypes.INTEGER,
             allowNull: false
         }
     });
 
     Model.associate = function (models) {
-        this.belongsTo(models.Section, { as: 'section', foreignKey: 'sectionId' });
+        this.belongsTo(models.SectionPage, { as: 'section', foreignKey: 'sectionPageId' });
     };
 
 
