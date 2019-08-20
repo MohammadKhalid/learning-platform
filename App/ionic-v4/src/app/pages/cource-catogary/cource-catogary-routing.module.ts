80be8279ba2eb08ddfec10594693af335ecae9f1
owaismmc <owais@mmcgbl.com>
2019-08-20 10:02:29 +0500

    course category routing issue resolve

--- App/ionic-v4/src/app/pages/cource-catogary/cource-catogary-routing.module.ts
+++ App/ionic-v4/src/app/pages/cource-catogary/cource-catogary-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+
+const routes: Routes = [
+  { path: '', loadChildren: './course-list/course-list.module#CourseListPageModule' },
+  { path: 'edit/:id', loadChildren: './cource-catogary.module#CourceCatogaryPageModule' },
+  { path: 'add', loadChildren: './cource-catogary.module#CourceCatogaryPageModule' },
+];
 @NgModule({
+
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild(routes)
   ]
 })
 export class CourceCatogaryRoutingModule { }
