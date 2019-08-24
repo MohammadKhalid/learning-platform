aa055352d375ae8aa7bc38ec2774f0b4a2202196
owaismmc <owais@mmcgbl.com>
2019-08-24 16:58:13 +0500

    question add view

--- App/ionic-v4/src/app/components/quiz-student/quiz-student.component.ts
+++ App/ionic-v4/src/app/components/quiz-student/quiz-student.component.ts
@@ -4,6 +4,7 @@ import { AuthenticationService } from 'src/app/services/user/authentication.serv
 import { Router } from '@angular/router';
 import { AddEditPopoverComponent } from '../common/add-edit-popover/add-edit-popover.component';
 import { PopoverController } from '@ionic/angular';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-quiz-student',
@@ -27,7 +28,7 @@ export class QuizStudentComponent implements OnInit {
     private auth: AuthenticationService,
     private router: Router,
     public popoverController: PopoverController,
-
+    private notificationService: NotificationService,
   ) { }
   // ionViewDidEnter() {
   //   // this.QuizIndexEitter.next();
@@ -35,11 +36,9 @@ export class QuizStudentComponent implements OnInit {
   ngOnInit() {
 
     this.user = this.auth.getSessionData().user;
-    this.data
-
     this.attempted = this.user.type === "coach" ? true : this.data.attempted;
     //  for (const item of this.data) {
-    
+
     this.quizzesArray.push({
       questionId: this.data.id,
       sectionPageId: this.data.sectionPageId,
@@ -111,17 +110,22 @@ export class QuizStudentComponent implements OnInit {
     return await popover.present();
   }
   submitQuiz(quizRow) {
-    quizRow.userId= this.user.id;
+    quizRow.userId = this.user.id;
     debugger;
     // this.attempted = true;
+    this.restApi.postPromise('quiz/submit-quiz', quizRow)
+      .then(response => {
+        debugger;
+        this.attempted = true;
+       
+        // this.quizzesArray.splice(this.quizzesArray.indexOf(quizRow), 1, quizRow);
+        // let index = this.quizzesArray.findIndex(item => item.id === quizRow.id);
+        // this.quizzesArray.splice(index, 1, response);
 
-    // this.restApi.postPromise('quiz', quizRow)
-    //   .then(response => {
-    //     this.attempted = true;
-    //     // this.router.navigate([`certification/sections/concepts/${this.sectionId}/${this.sectionPageId}`])
-    //     // this.notificationService.showMsg('Record Insert');
-    //   }).catch(err => {
-    //     // this.notificationService.showMsg(err);
-    //   })
+        this.notificationService.showMsg(response);
+      }).catch(err => {
+        debugger;
+        this.notificationService.showMsg(err);
+      })
   }
 }
